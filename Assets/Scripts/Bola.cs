6210894e727867f1dcc1c2a25733bd9865db5b9c
Eduard Ru xtec <eruesga@xtec.cat>
2020-11-25 19:09:04 +0100

    Feature
    Init project. Vsasset importet. Firsts lines

---
+++ Assets/Scripts/Bola.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour
+{
+
+    float speed;
+    // Start is called before the first frame update
+    float radius;
+    Vector2 direction;
+    void Start()
+    {
+        direction = Vector2.one.normalized;
+        radius = transform.localScale.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        if(transform.position.y < GameManager.bottomLeft.y + radius && direction.y < 0){
+            direction.y = -direction.y;
+        }
+        if(transform.position.y > GameManager.bottomRight.y + radius && direction.y > 0){
+            direction.y = -direction.y;
+        }
+
+        if (transform.position.x < GameManager.bottomRight.x + radius && direction.x)
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Bola bola;
+    public Barra barra;
+
+    public float BottomLeft;
+    public float TopRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate (bola);
+
+        Instantiate (barra);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
